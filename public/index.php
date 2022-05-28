e2255eaf01674787252a5a0c0f6d7e91bb5dc23c
skye7012 <skye7012@gmail.com>
2022-05-29 01:23:03 +0300

    fucking cors

--- public/index.php
+++ public/index.php
@@ -8,6 +8,22 @@ require __DIR__ . '/../config/db.php';
 
 $app = AppFactory::create();
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+	return $response;
+});
+
+$app->add(function ($request, $handler) {
+	$response = $handler->handle($request);
+	return $response
+			->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+			->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
 	$name = $args['name'];
 	$response->getBody()->write("Hello $name!");
