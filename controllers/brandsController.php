1ebd874f9db5301450025939e079604746cc3e34
skye7012 <skye7012@gmail.com>
2022-05-28 23:59:18 +0300

    trying

---
+++ controllers/brandsController.php
@@ -0,0 +1,44 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/brands', function (Request $request, Response $response) {
+	$sql = "SELECT * FROM brands";
+	$conn = DB::connect();
+	$query = pg_query($conn, $sql);
+	$brands = pg_fetch_all($query);
+
+	$response->getBody()->write(json_encode($brands));
+	return $response
+		->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/brands', function (Request $request, Response $response) {
+	$body = json_decode($request->getBody());
+	$name = $body->name;
+
+	$conn = DB::connect();
+	$res = pg_insert($conn,'public.brands',['name' => $name]);
+	$res = boolval($res);
+
+	$response->getBody()->write(json_encode($res));
+	return $response
+		->withHeader('Content-Type', 'application/json');
+});
+
+$app->put('/brands/{id}', function (Request $request, Response $response, $args) {
+	// $body = json_decode($request->getBody());
+	// $name = $body->name;
+
+	// $conn = DB::connect();
+	// $res = pg_update($conn,'public.brands',['name' => $name], ['id' => $args['id']]);
+	// $res = boolval($res);
+
+	// $response->getBody()->write(json_encode($res));
+	// return $response
+	// 	->withHeader('Content-Type', 'application/json');
+
+	$response->getBody()->write(json_encode("put"));
+	return $response
+		->withHeader('Content-Type', 'application/json');
+});
--- controllers/user.php
+++ controllers/user.php
@@ -3,18 +3,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../config/db.php';
-
-$app = AppFactory::create();
-
 $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
 	$name = $args['name'];
 	$response->getBody()->write("Hello $name!");
 	return $response;
 });
-
-//Brand controller
-require __DIR__ . '/../controllers/brandsController.php';
-
-$app->run();
